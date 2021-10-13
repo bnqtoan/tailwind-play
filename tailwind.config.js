77b86823a1821146094138d0e772ea30cee1483a
Toan Bui (Flexer) <bnqtoan@gmail.com>
2021-10-13 21:50:20 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  mode: 'jit',
+  purge: [
+    "**/*.html"
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+    },
+    fontFamily: {
+        "body": "'Inter', sans-serif"
+    },
+    extend: {
+      
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
